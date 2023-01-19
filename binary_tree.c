6f0a3121697e729f096855a53b5fb931d8b40ace
Anant Chitranshi <anantchitranshi001@gmail.com>
2023-01-19 19:38:20 +0530

    ALGORITHMS

---
+++ binary_tree.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+void construct_tree(struct node **node){
+    struct node *new_node;
+    int data, left, right;
+    new_node = malloc(sizeof(struct node));
+    printf("Enter data : ");
+    scanf("%d",&data);
+    printf("Enter left child : ");
+    scanf("%d",&left);
+    printf("Enter right child : ");
+    scanf("%d",&right);
+    *node = new_node;
+    new_node->data = data;
+
+    if (left == 1)
+    {
+        return construct_tree(&new_node->left);
+    }
+    
+    if (right == 1)
+    {
+        return construct_tree(&new_node->right);
+    }
+}
+
+int main(){
+    struct node *root_node = NULL;
+
+    construct_tree(&root_node);
+}
+
