b62829288630063c718a02bd06bd78be752608d8
Anant Chitranshi <anantchitranshi001@gmail.com>
2023-01-28 19:43:54 +0530

    ALGORITHMS

--- binary_tree.c
+++ binary_tree.c
@@ -49,9 +49,30 @@ void construct_tree(BINARY_NODE **node){
 
 void traverse(BINARY_NODE *node){
     BINARY_NODE *temp = node;
+    /*
+        The position of printf() will decide the type of traversal
+        Three types of traversal :
+            -> Inorder : 
+                Process the left subtree
+                Print the Root node
+                Process the rigth subtree
+            
+            -> Postorder : 
+                Process the left subtree
+                Process the rigth subtree
+                Print the Root node
+            
+            -> Preorder : 
+                Print the Root node
+                Process the left subtree 
+                Process the right subtree
+    */
+
     // printf("%d ",temp->data);
     // 1 2 3 4 5 6 7 
 
+
+    // Processing the left subtree 
     if (temp->left!=NULL)
     {
         traverse(temp->left);
@@ -60,6 +81,7 @@ void traverse(BINARY_NODE *node){
     // printf("%d ",temp->data);
     // 3 2 4 1 6 5 7 
 
+    // Processing the right subtree 
     if (temp->right!=NULL)
     {
         traverse(temp->right);
@@ -69,12 +91,16 @@ void traverse(BINARY_NODE *node){
     // 3 4 2 6 7 5 1
 }
 
+void height_tree(BINARY_NODE **node){
+    printf("Yess I am running baby !!");
+}
+
 int main(){
     BINARY_NODE *root_node = NULL;
 
     construct_tree(&root_node);
     traverse(root_node); 
-
+    height_tree(&root_node);
     return 0;
 }
 
